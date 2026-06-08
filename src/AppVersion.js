971e3201765564f79333f6a72d4660dcf4327c14
NguyenHop1909 <ng.vanhop09@gmail.com>
2026-06-08 13:22:16 +0700

    add nhieu thu

---
+++ src/AppVersion.js
@@ -0,0 +1,14 @@
+// File: AppVersion.js
+export const APP_CONFIG = {
+  currentVersion: "1.1.0",
+  features: [
+    "🎡 Vòng quay nhân phẩm: Em bé hã đặt vòng quay để anh ấy thử vận may của anh ấy nhé bé yêu ✨",
+    "🛠 Admin Panel: Góc quản lý riêng của bé, anh đã tối ưu cho bé dùng thật xịn xò. ⚙️",
+    "🌈 Trải nghiệm: Hiệu ứng mới mượt mà, giúp bé thao tác nhanh chóng và thoải mái nhất. 🚀",
+    "🎀 Pop-up xịn xò: Thông báo cập nhật phiên bản mới, anh dành riêng cho bé đó. 💌",
+    "📈 Cập nhật tức thì: Kết quả và quà tặng hiện ra ngay sau khi quay cho bé kiểm tra nha! 💓",
+    "⏳ Loading 'yêu': Hiệu ứng chờ đợi mới tinh, anh chỉ xin bé 'đợi anh xíu' thôi nè <3",
+    "🎯 Tùy chỉnh vòng quay: Bé có thể thay đổi điều kiện và phần thưởng tùy ý trong ô quay. 👑",
+    "Chúc bé yêu của anh luôn vui vẻ và hạnh phúc vf trải nghiệm phiên bản mới này nhé! 🥰",
+  ],
+};
