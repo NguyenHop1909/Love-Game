d76549a958339e3a570a9a66a1ae628254c3b00b
NguyenHop <ng.vanhop09@gmail.com>
2026-06-13 19:25:55 +0700

    update túi đồ đã ngon

--- src/AppVersion.js
+++ src/AppVersion.js
@@ -1,6 +1,6 @@
 // File: AppVersion.js
 export const APP_CONFIG = {
-  currentVersion: "1.1.1",
+  currentVersion: "1.1.2",
   features: [
     "Fix được backend túi đồ logic hơn rồi em bé ơi",
     "Chúc em bé trải nghiệm bản 1.1.1 thật vui vẻ và đừng quên gửi feedback về cho anh để anh còn cải thiện thêm nữa nha! ❤️"
